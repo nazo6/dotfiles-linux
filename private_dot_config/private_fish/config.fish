bd70e861b57e8c93ea8189bb8881691c5770f44a
nazo6 <git@nazo6.dev>
2025-05-10 00:15:03 +0900

    kitty ssh term

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -5,6 +5,7 @@ if type -q mise
 end
 
 if status is-interactive
+  # shell extensions
   if type -q zoxide
     zoxide init fish | source
   end
@@ -12,6 +13,7 @@ if status is-interactive
     starship init fish | source
   end
 
+  # aliases
   abbr -a n nvim
   abbr -a lg lazygit
   abbr -a fish-reload 'source ~/.config/fish/**/*.fish'
@@ -19,5 +21,15 @@ if status is-interactive
   if type -q eza
     alias ls='eza'
     alias ll='eza -la'
+  end
+
+  # env
+  if type -q nvim
+    set -x EDITOR nvim
+  end
+
+  # If terminal is kitty, open ssh with TERM=xterm option
+  if test "$TERM" = "xterm-kitty"
+    alias ssh='TERM=xterm command ssh'
   end
 end
